f7db0515c600b0f452c8d22bed2e6ec408973719
Roman Nozdrachev <roman.nozdracheff@yandex.ru>
2021-10-02 21:25:13 +0300

    добавил описание интерфейсов

--- src/main/java/crossword/example/data/WordReader.java
+++ src/main/java/crossword/example/data/WordReader.java
@@ -2,10 +2,14 @@ package crossword.example.data;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public interface WordReader {
 
-
+    /**
+     *
+     * @return словарь рассортированный по длине слов
+     *Integer - длина слов
+     * List<String> - список слов с данной длиной
+     */
     Map<Integer, List<String>> getWords();
 }
--- src/main/java/crossword/example/service/SolvingCrossword.java
+++ src/main/java/crossword/example/service/SolvingCrossword.java
@@ -4,5 +4,11 @@ import java.util.List;
 
 public interface SolvingCrossword {
 
+    /**
+     *
+     * @param wordToGuess - слово с пропущенными буквами по которому мы ищем совпадающие слова
+     * @return список слов,которые соответствуют длине wordToGuess и значения
+     * букв которых совпадают с известными буквами wordToGuess
+     */
     List<String> getOptions(String wordToGuess);
 }
